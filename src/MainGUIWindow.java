9065adca1360524ba09c8d1f9e83973db49380a7
BT_3W2_28 <annac101@nycstudents.net>
2023-05-31 09:29:18 -0400

    3

---
+++ src/MainGUIWindow.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MainGUIWindow extends JFrame implements ActionListener{
+    private JPanel mainPanel;
+    private JButton loadTimelineButton;
+    private JButton searchByPeopleButton;
+    private JButton searchByEventButton;
+    private JButton addYourOwnEventButton;
+    private JLabel welcome;
+
+    public MainGUIWindow(){
+        createUIComponents();
+    }
+
+    private void createUIComponents(){
+        setContentPane(mainPanel);
+        setTitle("Tip Calculate");
+        setSize(700,400);
+        setLocation(450,100);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        loadTimelineButton.addActionListener(this);
+        searchByEventButton.addActionListener(this);
+        searchByPeopleButton.addActionListener(this);
+        addYourOwnEventButton.addActionListener(this);
+        this.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source instanceof JButton) {
+            JButton button = (JButton) source;
+            String text = button.getText();
+            if (button == loadTimelineButton) {
+                setContentPane(NewGUI);
+            }
+            if (button == searchByEventButton) {
+
+            }
+            if (button == searchByPeopleButton) {
+
+            }
+            if (button == addYourOwnEventButton) {
+
+            }
+
+        }
+    }
+    }
+
