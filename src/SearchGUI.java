35a5a2c35dc60b73b7f44ef2a8f082c5a78cf752
BT_3W2_28 <annac101@nycstudents.net>
2023-06-07 09:34:18 -0400

    6

--- src/SearchGUI.java
+++ src/SearchGUI.java
@@ -7,9 +7,11 @@ public class SearchGUI extends JFrame implements ActionListener {
     private JButton backButton;
     private JPanel mainPanel;
     private ArrayList<Timeline> times;
+    private ArrayList<JButton> buttons;
 
     public SearchGUI(ArrayList<Timeline> times) {
         this.times = times;
+        ArrayList<JButton> buttons = new ArrayList<JButton>();
         createUIComponents();
 
     }
@@ -20,16 +22,24 @@ public class SearchGUI extends JFrame implements ActionListener {
         setSize(700, 700);
         setLocation(450, 100);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(null);
+        mainPanel.setLayout(null);
         backButton.addActionListener(this);
         this.setVisible(true);
-        int x = 20;
+        int x = -20;
         int y = 180;
         for(int i = 0;i<times.size();i++){
+            System.out.println(times.get(i).getName());
             JButton ne = new JButton();
-            ne.setText(""+times.get(i).getYear()+" "+times.get(i).getName());
+            ne.setText(""+times.get(i).getYear()+" "+times.get(i).getName()+times.size());
             ne.addActionListener(this);
-            ne.setSize(700,x+50);
+            //ne.setSize(700,70);
+            ne.setLayout(null);
+            ne.setBounds(0,y+30,700,70);
+            x+=50;
+            y+=70;
             mainPanel.add(ne);
+            buttons.add(ne);
         }
     }
 
@@ -40,6 +50,9 @@ public class SearchGUI extends JFrame implements ActionListener {
             if (button == backButton) {
                 this.dispose();
                 PeopleGUI myWindow = new PeopleGUI(true);
+            }
+            else{
+                //for(int i=0; i<buttons)
             }
 
 
